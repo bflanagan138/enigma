8f4d2006bf1551f4fc77fcd128c01c9c2e0665b1
Bryan Flanagan <bflanagan78@yahoo.com>
2022-11-11 15:24:52 -0700

    refactor: Fix Enigma initialize tests

--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -7,9 +7,9 @@ RSpec.describe Enigma do
   end
   it 'exists and has 27 characters' do
     expect(@enigma).to be_a (Enigma)
-    expect(@enigma.alphabet.count).to eq (27)
-    expect(@enigma.first).to eq ("a")
-    expect(@enigma.last).to eq (" ")
+    expect(@enigma.character_set.count).to eq (27)
+    expect(@enigma.character_set.first).to eq ("a")
+    expect(@enigma.character_set.last).to eq (" ")
   end
 
   it 'generates a random five digit number' do
@@ -23,8 +23,6 @@ RSpec.describe Enigma do
     expect(@enigma.todays_date).to be_a (String)
     expect(@enigma.todays_date.length).to eq (6)
   end
-
-  it 'returns '
 
   xit 'can encrypt a message' do
     expect(@enigma.encrypt("hello world", "02715", "040895")).to eq ({encryption: "keder ohulw", key: "02715", date: "040895"})
