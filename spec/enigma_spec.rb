ff8b79ddce795a6cd3d8026d49d94598ebb1b715
Bryan Flanagan <bflanagan78@yahoo.com>
2022-11-10 19:42:34 -0700

    feat: Add test for key generator

--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -2,13 +2,20 @@ require 'date'
 require './lib/enigma'
 
 RSpec.describe Enigma do
+  before(:each) do
+    @enigma = Enigma.new
+  end
   it 'exists' do
-    enigma = Enigma.new
-    expect(enigma).to be_a (Enigma)
+    expect(@enigma).to be_a (Enigma)
   end
 
-  it 'can encrypt a message' do
-    enigma = Enigma.new
-    expect(enigma.encrypt("hello world", "02715", "040895")).to eq ({encryption: "keder ohulw", key: "02715", date: "040895"})
+  it 'generates a random five digit number' do
+    expect(@enigma.key_generator.length).to eq (5)
   end
-end
+
+  xit 'can encrypt a message' do
+    expect(@enigma.encrypt("hello world", "02715", "040895")).to eq ({encryption: "keder ohulw", key: "02715", date: "040895"})
+  end
+
+  
+end 
