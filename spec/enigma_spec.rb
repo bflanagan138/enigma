9e3846ef5f87b98e626863c17d81fa88f175ecbd
Bryan Flanagan <bflanagan78@yahoo.com>
2022-11-10 20:03:41 -0700

    feat: Write todays date tests

--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -14,6 +14,11 @@ RSpec.describe Enigma do
     expect(@enigma.key_generator.length).to eq (5)
   end
 
+  it 'returns todays date as a string' do
+    expect(@enigma.todays_date).to be_a (String)
+    expect(@enigma.todays_date.length).to eq (6)
+  end
+
   xit 'can encrypt a message' do
     expect(@enigma.encrypt("hello world", "02715", "040895")).to eq ({encryption: "keder ohulw", key: "02715", date: "040895"})
   end
