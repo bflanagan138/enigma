891d3bdc5ea02b978b17fc6bfa1e53a729542552
Bryan Flanagan <bflanagan78@yahoo.com>
2022-11-11 15:54:47 -0700

    refactor: Fix key_generator method to always return 5 digits

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,18 +1,29 @@
 class Enigma
-  attr_reader :character_set
+  attr_reader :character_set, 
+              :keys
   def initialize
     @character_set = ("a".."z").to_a << " "
+    @keys = []
   end
 
   #helper method for encrypt
   def key_generator
-    rand(99999).to_s
+    rand(99999).to_s.rjust(5, "0")
   end
   
   #helper method for encrypt
   def todays_date
     Date.today.strftime("%D").delete("/")
   end
+  
+  def keys
+    require 'pry'; binding.pry
+  end
+  #shift conventions
+    #a_shift = a_key + a_offset
+    #b_shift = b_key + b_offset
+    #c_shift = c_key + c_offset
+    #d_shift = d_key + d_offset
 
   def encrypt(message, shift = key_generator, offset = todays_date)
     message.split('')
