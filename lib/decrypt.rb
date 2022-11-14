c15d00302fad15edcf0cf04a15ae32e618f125d3
Bryan Flanagan <bflanagan78@yahoo.com>
2022-11-14 16:24:17 -0700

    WIP: Refactor encrypt and decrypt rb files to output according to iteration pattern

--- lib/decrypt.rb
+++ lib/decrypt.rb
@@ -2,10 +2,12 @@ require_relative 'enigma'
 require 'date'
 
 message = File.open(ARGV[0], "r")
+key = ARGV[2]
+date = ARGV[3]
 encrypted_message = message.read
 enigma = Enigma.new
-decrypted_message = enigma.decrypt(encrypted_message, '02714', '040895')
+decrypted_message = enigma.decrypt(encrypted_message, key, date)
 
 writer = File.open(ARGV[1], "w")
 writer.write(decrypted_message[:decryption])
-puts "Created #{ARGV[1]} with the key #{enigma.key} and date #{enigma.offsets} } "
+puts "Created #{ARGV[1]} with the key #{decrypted_message[:key]} and date #{decrypted_message[:date]}"
--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -1,12 +1,11 @@
 require_relative 'enigma'
+require 'date'
 
-message = File.open(ARGV[0], "r")
-key = ARGV[2].to_s 
-date = ARGV[3].to_s
-incoming_message = message.read
+incoming_message = File.open(ARGV[0], "r")
+message = incoming_message.read
 enigma = Enigma.new
-encrypted_message = enigma.encrypt(incoming_message, key, date)
+encrypted_message = enigma.encrypt(message)
 
 writer = File.open(ARGV[1], "w")
 writer.write(encrypted_message[:encryption])
-puts "created #{ARGV[1]} with the key #{encrypted_message[:key]} and date #{encrypted_message[:date]}"
+puts "Created #{ARGV[1]} with the key #{encrypted_message[:key]} and date #{encrypted_message[:date]}"
