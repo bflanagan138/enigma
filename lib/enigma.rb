7e463a6a0744347615ebc2f7ed37f670d270170f
Bryan Flanagan <bflanagan78@yahoo.com>
2022-11-13 18:12:27 -0700

    refactor: Update message_to_char_index method to remove ASCII numbers

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -32,23 +32,34 @@ class Enigma
   end
 
   def convert_offset(number)
-    conversion = ((number.to_i ** 2) % 10000).to_s.chars.map(&:to_i)
+    ((number.to_i ** 2) % 10000).to_s.chars.map(&:to_i)
   end
 
   def final_shift
     [key_to_four_pairs, offsets].transpose.map(&:sum)
-    
   end
 
   def message_to_char_index(message)
-    message.downcase.bytes.map do |letter_to_ascii_char|
-      if letter_to_ascii_char == 32
-        letter_to_ascii_char - 6
-      elsif (97..122).include?(letter_to_ascii_char)
-        letter_to_ascii_char - 97
+    # require 'pry'; binding.pry
+    message.downcase.split("").map do |character|
+      # require 'pry'; binding.pry
+      if !@character_set.include?(character)
+        character
+      else 
+        @character_set.find_index(character)
       end
     end
   end
+
+
+  #   message.downcase.bytes.map do |letter_to_ascii_char|
+  #     if letter_to_ascii_char == 32
+  #       letter_to_ascii_char - 6
+  #     elsif (97..122).include?(letter_to_ascii_char)
+  #       letter_to_ascii_char - 97
+  #     end
+  #   end
+  # end
 
   def shift_number(number, shift_number)
     (number + shift_number) % 27
@@ -70,6 +81,29 @@ class Enigma
           convert_offset(offsets)
         end
     message_to_numeric = message_to_char_index(message)
+    message_char_shift = []
+    message_to_numeric.each.with_index do |number, index|
+      message_char_shift << shift_number(number, final_shift[index % 4])
+    end
+
+    encrypted_message = message_char_shift.map do |char_index|
+      @character_set[char_index]
+    end.join
+    {
+      encryption: encrypted_message,
+      key: @key,
+      date: offsets
+    }
+  end
+
+  def decrypt(message, offsets = '')
+    @offsets =
+      if offsets == ''
+        convert_offset(todays_date)
+      else
+        convert_offset(offsets)
+      end
+    decrypt_to_numeric = message_to_char_index(message)
     message_char_shift = []
     message_to_numeric.each.with_index do |number, index|
       message_char_shift << shift_number(number, final_shift[index % 4])
