0655a59370858314bc4fe1d80409b386914b9f34
Bryan Flanagan <bflanagan78@yahoo.com>
2022-11-14 15:57:05 -0700

    refactor: Update encrypt file to meet iteration pattern specs

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -1,10 +1,11 @@
 require_relative 'enigma'
-require 'date'
 
 message = File.open(ARGV[0], "r")
+key = ARGV[2].to_s 
+date = ARGV[3].to_s
 incoming_message = message.read
 enigma = Enigma.new
-encrypted_message = enigma.encrypt(incoming_message, '02715', '040895')
+encrypted_message = enigma.encrypt(incoming_message, key, date)
 
 writer = File.open(ARGV[1], "w")
 writer.write(encrypted_message[:encryption])
