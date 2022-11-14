5a527e5d0f3035bf9ec6df85ccd73cb88fd0c6a1
Bryan Flanagan <bflanagan78@yahoo.com>
2022-11-13 18:15:40 -0700

    refactor: Update  message_to_char_index test to test for special characters

--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -5,17 +5,18 @@ RSpec.describe Enigma do
   before(:each) do
     @enigma = Enigma.new
   end
-  it 'exists and has 27 characters' do
+  it 'exists and has attributes' do
     expect(@enigma).to be_a (Enigma)
     expect(@enigma.character_set.count).to eq (27)
     expect(@enigma.character_set.first).to eq ("a")
     expect(@enigma.character_set.last).to eq (" ")
+    expect(@enigma.key).to eq nil
+    expect(@enigma.offsets).to eq nil
   end
 
   it 'generates a random five digit number' do
     expect(@enigma.key_generator).to be_a String
     expect(@enigma.key_generator.length).to eq (5)
-    
   end
 
   it 'creates an array of 4 key strings from the return value of the key generator' do
@@ -51,8 +52,8 @@ RSpec.describe Enigma do
     expect(@enigma.message_to_char_index('a')).to eq [0]
     expect(@enigma.message_to_char_index(' ')).to eq [26]
     expect(@enigma.message_to_char_index('a ')).to eq [0, 26]
-    expect(@enigma.message_to_char_index('A')).to eq [0]
     expect(@enigma.message_to_char_index('AB')).to eq [0, 1]
+    expect(@enigma.message_to_char_index('!?89')).to eq ['!', '?', '8', '9']
   end
 
   it 'can shift a number' do
@@ -66,10 +67,13 @@ RSpec.describe Enigma do
       key: '02715',
       date: '040895'
     })
-    # expect(@enigma.encrypt("hello! world!", "02715", "040895")).to eq ({
-      #encryption: "keder! ohulw!", 
-      #key: "02715", 
-      #date: "040895"
-    #})
+  end
+
+  xit 'can decrypt a message' do
+    expect(@enigma.decrypt('keder ohulw', '02715', '040895')).to eq ({
+      encryption: 'hello world',
+      key: '02715',
+      date: '040895'
+    })
   end
 end
