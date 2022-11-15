063d4f737375665820fa2568668d9ba4630afb95
Bryan Flanagan <bflanagan78@yahoo.com>
2022-11-14 21:55:29 -0700

    refactor: Fix encrypt to return date

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -4,8 +4,14 @@ require 'date'
 incoming_message = File.open(ARGV[0], "r")
 message = incoming_message.read
 enigma = Enigma.new
-encrypted_message = enigma.encrypt(message)
+encrypted_message = enigma.encrypt(message, '02715', '040895')
+todays_date = Date.today.strftime("%D").delete("/")
 
 writer = File.open(ARGV[1], "w")
 writer.write(encrypted_message[:encryption])
-puts "Created #{ARGV[1]} with the key #{encrypted_message[:key]} and date #{encrypted_message[:date]}"
+# require 'pry'; binding.pry
+if encrypted_message[:date] == ''
+  puts "Created #{ARGV[1]} with the key #{encrypted_message[:key]} and date #{todays_date}"
+else
+  puts "Created #{ARGV[1]} with the key #{encrypted_message[:key]} and date #{encrypted_message[:date]}"
+end
