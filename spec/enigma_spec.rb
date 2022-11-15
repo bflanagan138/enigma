da482a97bebc753786d99afecaf0b858d8ba938c
Bryan Flanagan <bflanagan78@yahoo.com>
2022-11-15 15:20:45 -0700

    refactor: Update spec files

--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -9,6 +9,21 @@ RSpec.describe Enigma do
   
   it 'exists' do
     expect(@enigma).to be_a (Enigma)
+    expect(@enigma.character_set.count).to eq (27)
+    expect(@enigma.character_set.first).to eq ("a")
+    expect(@enigma.character_set.last).to eq (" ")
+  end
+
+  it 'creates an array of 4 key strings from the key' do
+    expect(@enigma.key_to_four_pairs.length).to eq (4)
+    expect(@enigma.key_to_four_pairs).to be_a (Array)
+    four_keys = []
+    four_keys << @enigma.key[0..1].to_i + @enigma.convert_offset[0]
+    four_keys << @enigma.key[1..2].to_i + @enigma.convert_offset[1]
+    four_keys << @enigma.key[2..3].to_i + @enigma.convert_offset[2]
+    four_keys << @enigma.key[3..4].to_i + @enigma.convert_offset[3]
+    expect(@enigma.key_to_four_pairs).to eq four_keys
+    expect(@enigma.key_to_four_pairs).to be_a Array
   end
 
   it 'can convert message to index numbers in alphabet' do
