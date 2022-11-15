fded66a5d404198287c983a27631062902c67990
Bryan Flanagan <bflanagan78@yahoo.com>
2022-11-15 11:09:16 -0700

    refactor: Encrypt.rb

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -1,14 +1,14 @@
 require_relative 'enigma'
 require 'date'
 
-incoming_message = File.open(ARGV[0], "r")
-message = incoming_message.read
+incoming_message = File.read(ARGV[0])
+# message = incoming_message.read
 enigma = Enigma.new
-encrypted_message = enigma.encrypt(message, '02715', '040895')
+encrypted_message = enigma.encrypt(incoming_message, '02715', '040895')
 todays_date = Date.today.strftime("%D").delete("/")
 
-writer = File.open(ARGV[1], "w")
-writer.write(encrypted_message[:encryption])
+writer = File.write(ARGV[1], encrypted_message[:encryption])
+# writer.write(encrypted_message[:encryption])
 # require 'pry'; binding.pry
 if encrypted_message[:date] == ''
   puts "Created #{ARGV[1]} with the key #{encrypted_message[:key]} and date #{todays_date}"
